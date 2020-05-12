244e0ceebb8c5ebc85581529e1161e5772274e2c
FudgeCookie <32051513+FudgeCookie@users.noreply.github.com>
2020-05-12 00:46:27 -0400

    Update setupUI.m

--- setupUI.m
+++ setupUI.m
@@ -19,6 +19,7 @@ classdef setupUI < matlab.apps.AppBase
 
 
     properties (Access = private)
+        % Initialize variables that track properties of various environment components
         wallCount = 0;
         currentObj = {};
         currentObjLen = 0;
@@ -27,6 +28,7 @@ classdef setupUI < matlab.apps.AppBase
         sNode = {};
         gNode = {};
 
+        % Initialize variables that describe the state of the setup UI 
         placeMode = 1;
         axesScale = 10;
         changesMade = false;
@@ -36,22 +38,31 @@ classdef setupUI < matlab.apps.AppBase
     methods (Access = private)
 
         function plotCurrentObj(app)
+            % Each object is defined by a 2 x K matrix that stores that objects vertices
+            % The ith and (i+1)th vertices within an object's defining matrix are connected 
+            % by a line which denotes a wall
             if app.currentObjLen > 0
                 currentObjPlotArray = cell2mat(app.currentObj);
+                % Plot each object in currentObj, which stores the object currently being edited
                 plot(app.EnvAxes, currentObjPlotArray(1,:), currentObjPlotArray(2,:), 'ro-', 'LineWidth', 2);
             end
         end
 
         function plotObjs(app)
+            % Each object is defined by a 2 x K matrix that stores that objects vertices
+            % The ith and (i+1)th vertices within an object's defining matrix are connected 
+            % by a line which denotes a wall
             hold(app.EnvAxes, 'on');
             for i = 1:app.objsLen
                 thisObjPlotArray = app.objs{1, i};
+                % Plot each object in objs, which stores all previously completed objects
                 plot(app.EnvAxes, thisObjPlotArray(1,:), thisObjPlotArray(2,:), 'b-', 'LineWidth', 2);
             end
             hold(app.EnvAxes, 'off');
         end
 
         function plotSNode(app)
+            % Plot the starting node
             sz = size(app.sNode);
             r = app.axesScale*0.05;
             mr = r/1.5;
@@ -68,6 +79,7 @@ classdef setupUI < matlab.apps.AppBase
         end
 
         function plotGNode(app)
+            % Plot the goal node
             sz = size(app.gNode);
             r = app.axesScale*0.05;
             mr = r/1.5;
@@ -87,6 +99,8 @@ classdef setupUI < matlab.apps.AppBase
     methods (Access = public)
 
         function setParentUI(app, parent)
+            % Set this parent UI of this instance of setup UI, so that the push button can 
+            % return the newly configured envirnoment when the user is done editing
             app.parentUI = parent;
         end
     end
@@ -97,51 +111,70 @@ classdef setupUI < matlab.apps.AppBase
 
         % Value changed function: WallButton
         function WallButtonValueChanged(app, event)
+            % What to do when the wall button is selected
             value = app.WallButton.Value;
             if (value)
+                % The first two lines set the states of the other two buttons to false, since
+                % the user can only place one type of object at a time
                 app.StartingNodeButton.Value = 0;
                 app.GoalNodeButton.Value = 0;
-                app.placeMode = 1;
+                app.placeMode = 1; % placeMode 1 = place wall vertices on mouse click
             end
         end
 
         % Value changed function: StartingNodeButton
         function StartingNodeButtonValueChanged(app, event)
+            % What to do when the starting node button is selected
             value = app.StartingNodeButton.Value;
             if (value)
+                % The first two lines set the states of the other two buttons to false, since
+                % the user can only place one type of object at a time
                 app.WallButton.Value = 0;
                 app.GoalNodeButton.Value = 0;
-                app.placeMode = 2;
+                app.placeMode = 2; % placeMode 2 = place starting node on mouse click
             end
         end
 
         % Value changed function: GoalNodeButton
         function GoalNodeButtonValueChanged(app, event)
+            % What to do when the goal node button is selected
             value = app.GoalNodeButton.Value;
             if (value)
+                % The first two lines set the states of the other two buttons to false, since
+                % the user can only place one type of object at a time
                 app.WallButton.Value = 0;
                 app.StartingNodeButton.Value = 0;
-                app.placeMode = 3;
+                app.placeMode = 3; % placeMode 3 = place goal node on mouse click
             end
         end
 
         % Button pushed function: FinishObstacleButton
         function FinishObstacleButtonPushed(app, event)
+            % What to do when the finish obstacle button is pressed
+            % Clicking this button finalizes the current object that the user is working on,
+            % and pushes it into objs (the list of all completed objects). currentObj will 
+            % then be reset to an empty state, so that the user can construct a new object
             if app.currentObjLen > 1
-                app.changesMade = true;
-                app.objsLen = app.objsLen + 1;
-                app.objs{1, app.objsLen} = cell2mat(app.currentObj);
+                app.changesMade = true; 
+                app.objsLen = app.objsLen + 1; 
+                app.objs{1, app.objsLen} = cell2mat(app.currentObj); % Add the current object to the list of completed objects
                 app.currentObjLen = 0;
-                app.currentObj = {};
-                app.EnvAxes.cla();
+                app.currentObj = {}; % Clear currentObj
+                app.EnvAxes.cla(); % Clear the axes so that the updated environment can be replotted
+                % Replot all components
                 app.plotObjs();
                 app.plotSNode();
                 app.plotGNode();
-                app.Objects0Label.Text = append('Objects: ',num2str(app.objsLen));
+                app.Objects0Label.Text = append('Objects: ',num2str(app.objsLen)); % Update the current number of objects
             elseif app.currentObjLen > 0
+                % This code can only be reached when currentObjLen = 1, i.e. when the current object is composed
+                % of exactly one point. This object is meaningless, since it cannot form a wall (or take up any space
+                % at all, for that matter), and therefore, instead of being added to the list of completed objects, the
+                % current object is simply deleted.
                 app.currentObjLen = 0;
-                app.currentObj = {};
-                app.EnvAxes.cla();
+                app.currentObj = {}; % Clear currentObj
+                app.EnvAxes.cla(); % Clear the axes so that the updated environment can be replotted
+                % Replot all components
                 app.plotObjs();
                 app.plotSNode();
                 app.plotGNode();
@@ -154,6 +187,7 @@ classdef setupUI < matlab.apps.AppBase
             if app.objsLen > 0
                 app.changesMade = true;
             end
+            % Delete all current/completed objects and components
             app.wallCount = 0;
             app.currentObj = {};
             app.currentObjLen = 0;
@@ -162,41 +196,53 @@ classdef setupUI < matlab.apps.AppBase
             app.sNode = {};
             app.gNode = {};
             app.wallCount = 0;
+            % Update wall and object count displays to reflect the above changes
             app.Walls0Label.Text = append('Walls: ',num2str(app.wallCount));
             app.Objects0Label.Text = append('Objects: ',num2str(app.objsLen));
         end
 
         % Button pushed function: PushButton
         function PushButtonPushed(app, event)
-            % Do whatever you need to in order to push
             if app.changesMade
-                app.parentUI.setComponents(app.objs, app.sNode, app.gNode, app.wallCount);
+                % If changes are made to the environment, then the parent UI's (aka the main UI) environment 
+                % is updated and the simulation in the parent UI reset. If not, then nothing happens, and 
+                % the setup UI is closed below
+                app.parentUI.setComponents(app.objs, app.sNode, app.gNode, app.wallCount); % Pass new component data to the parent UI
                 app.parentUI.reset();
             end
-            app.delete();
+            app.delete(); % Close the setup UI once the new changes are pushed to main
         end
 
         % Window button down function: UIFigure
         function UIFigureWindowButtonDown(app, event)
-            mousePos = get(app.EnvAxes,'CurrentPoint');
-            mousePos = mousePos(1, 1:2)';
+            % What to do when the left mouse button is pressed (mouse click event callback)
+            mousePos = get(app.EnvAxes,'CurrentPoint'); % Get mouse position with respect to EnvAxes
+            mousePos = mousePos(1, 1:2)'; % Consider only the x and y coordinates of the mouse press
 
             if mousePos >= 0 & mousePos <= 10
                 if app.placeMode == 1 % Wall/obj place mode
                     if app.currentObjLen > 0
-                        app.wallCount = app.wallCount + 1;
+                        app.wallCount = app.wallCount + 1; % Walls need two points to be defined, therefore we only 
+                        % update the number of walls if we aleady have at least one point in the current object
                         for i = 1:app.currentObjLen
+                            % If there exist any vertices in the current object, we iterate through them
                             thisPoint = cell2mat(app.currentObj);
                             thisPoint = thisPoint(:,i);
                             if sqrt(sum((mousePos - thisPoint).^2)) < app.axesScale * 0.01
-                                mousePos = thisPoint;
-                                break
+                                % For each point, determine if the mouse position at the moment of the click event
+                                % was within R units of point i , where R =  0.01 * (the width of the EnvAxes)
+                                mousePos = thisPoint; % If the above is true, we "snap" the new point to the position
+                                % of point i by setting mousePos to the coordinates of point i (since the new point 
+                                % that will be added to the current object will be placed at mousePos. 
+                                
+                                % The purpose of this check is to allow the user to easily create closed objects
+                                break % Break the loop, since we found a point that the mousePos could "snap" to
                             end
                         end
                     end
 
-                    app.currentObjLen = app.currentObjLen + 1;
-                    app.currentObj{1, app.currentObjLen} = mousePos;
+                    app.currentObjLen = app.currentObjLen + 1; % Update the length of currentObj to reflect a new point being added
+                    app.currentObj{1, app.currentObjLen} = mousePos; % Add a new point to currentObj, located at mousePos
                 elseif app.placeMode == 2
                     app.sNode{1, 1} = mousePos; % Starting Node place mode
                     app.changesMade = true;
@@ -205,11 +251,13 @@ classdef setupUI < matlab.apps.AppBase
                     app.changesMade = true;
                 end
 
-                app.EnvAxes.cla();
+                app.EnvAxes.cla(); % Clear the axes so that the updated environment can be replotted
+                % Replot all components
                 app.plotCurrentObj();
                 app.plotObjs();
                 app.plotSNode();
                 app.plotGNode();
+                % Update wall and object count displays to reflect the above changes
                 app.Walls0Label.Text = append('Walls: ',num2str(app.wallCount));
                 app.Objects0Label.Text = append('Objects: ',num2str(app.objsLen));
             end
@@ -341,14 +389,20 @@ classdef setupUI < matlab.apps.AppBase
         end
 
         function setComponents(app, newList, newSNode, newGNode, newWallCount)
+            % This function is called by the parent UI when an instance of setup UI is created
+            
+            % Pass in the current state of the parent UI's environment
             app.objs = newList;
             app.sNode = newSNode;
             app.gNode = newGNode;
+            % Update wall and object counts to reflect the above changes
             app.wallCount = newWallCount;
             [~, app.objsLen] = size(app.objs);
+            % Plot the environment
             app.plotObjs();
             app.plotSNode();
             app.plotGNode();
+            % Update wall and object count displays to reflect the above changes
             app.Walls0Label.Text = append('Walls: ',num2str(app.wallCount));
             app.Objects0Label.Text = append('Objects: ',num2str(app.objsLen));
         end
